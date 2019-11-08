78b01ec25680deed5bea5cc8c7abec7ae4028ff3
Justin Yum <justinyum98@gmail.com>
2019-11-07 19:50:32 -0800

    began adding Projects to About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,8 @@ import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
+import Project from '../Project/Project';
+
 const useStyles = makeStyles({
   avatar: {
     margin: 10,
@@ -18,23 +20,31 @@ export default function About() {
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <Typography variant="body1" align="center" gutterBottom>
-        Hello, my name is
-      </Typography>
-      <Avatar className={classes.avatar}>JY</Avatar>
-      <Typography variant="h6" align="center" gutterBottom>
-        Justin Yum
-      </Typography>
-      <Typography variant="body1" align="center" gutterBottom>
-        I am a web developer based in San Diego, California.
-      </Typography>
-      <Typography variant="body1" align="center" gutterBottom>
-        My passion is expressing myself creatively
-        <br />
-        in coding and music production, which are two
-        <br />
-        enjoyable pastimes of mine.
-      </Typography>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="body1" align="center" gutterBottom>
+          Hello, my name is
+        </Typography>
+        <Avatar className={classes.avatar}>JY</Avatar>
+        <Typography variant="h6" align="center" gutterBottom>
+          Justin Yum
+        </Typography>
+        <Typography variant="body1" align="center" gutterBottom>
+          I am a web developer based in San Diego, California.
+        </Typography>
+        <Typography variant="body1" align="center" gutterBottom>
+          My passion is expressing myself creatively
+          <br />
+          in coding and music production, which are two
+          <br />
+          enjoyable pastimes of mine.
+        </Typography>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h3" align="center" gutterBottom>
+          Projects
+        </Typography>
+        <Project />
+      </Box>
     </Box>
   );
 }
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Box from '@material-ui/core/Box';
+
+function Project() {
+  return (
+    <div>
+      Project component
+    </div>
+  );
+}
+
+export default Project;
