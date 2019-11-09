47dd5e44dd852af29418bba5ac1b85ee705a7a45
Justin Yum <justinyum98@gmail.com>
2019-11-08 16:37:00 -0800

    changed Box to Grid on the Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Typewriter from 'typewriter-effect';
 
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
@@ -19,38 +20,47 @@ export default function Home() {
 
   return (
     <div>
-      <Typography component="div" variant="h1" align="center" gutterBottom>
-        <Box fontFamily="Monospace" mt={20}>
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter.typeString('Justin Yum')
-                .start();
-            }}
-          />
-        </Box>
-      </Typography>
-      <Typography component="div" variant="h5" align="center" gutterBottom>
-        <Box fontFamily="Monospace">
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter.typeString('Developer. Producer. Artist.')
-                .start();
-            }}
-          />
-        </Box>
-      </Typography>
-      <Fade in={fadeOn} timeout={5000}>
-        <Box>
-          <Box display="flex" justifyContent="center">
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <Typography variant="h1" gutterBottom>
+            <Box fontFamily="Monospace" mt={20}>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString('Justin Yum')
+                    .start();
+                }}
+              />
+            </Box>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5" gutterBottom>
+            <Box fontFamily="Monospace">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString('Developer. Producer. Artist.')
+                    .start();
+                }}
+              />
+            </Box>
+          </Typography>
+        </Grid>
+        <Fade in={fadeOn} timeout={5000}>
+          <Grid item xs={12}>
             <IconButton href="https://soundcloud.com/justin-yum">
               <Soundcloud />
             </IconButton>
             <IconButton href="https://www.instagram.com/justinyum98/">
               <Instagram />
             </IconButton>
-          </Box>
-        </Box>
-      </Fade>
+          </Grid>
+        </Fade>
+      </Grid>
     </div>
   );
 }
