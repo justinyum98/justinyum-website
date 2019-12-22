a9512af4801dfa0e6ec33997ef57bf75f6c84bcc
Justin Yum <justinyum98@gmail.com>
2019-12-21 20:14:26 -0800

    added more playlists to music section

--- src/components/Music/index.jsx
+++ src/components/Music/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 export default function Music() {
   return (
-    <div>
-      <iframe title="music-playlist" src="https://open.spotify.com/embed/playlist/1XL2FemPhVeUz93X6grAbK" width="300" height="380" frameBorder="0" allowTransparency="true" allow="encrypted-media" />
-    </div>
+    <Grid container justify="space-evenly" spacing={2}>
+      <Grid item>
+        <iframe title="question-mark" src="https://open.spotify.com/embed/playlist/6wn4ohSyY4XYbzSn8qnlsC" width="300" height="380" frameBorder="0" allowTransparency="true" allow="encrypted-media" />
+      </Grid>
+      <Grid item>
+        <iframe title="current-mood" src="https://open.spotify.com/embed/playlist/1XL2FemPhVeUz93X6grAbK" width="300" height="380" frameBorder="0" allowTransparency="true" allow="encrypted-media" />
+      </Grid>
+      <Grid item>
+        <iframe title="top-songs-2019" src="https://open.spotify.com/embed/playlist/37i9dQZF1EtgalxKMhvoSY" width="300" height="380" frameBorder="0" allowTransparency="true" allow="encrypted-media" />
+      </Grid>
+      <Grid item>
+        <iframe title="play-funk-john" src="https://open.spotify.com/embed/playlist/4q3n9NCcuKla1tR1oJZ1s7" width="300" height="380" frameBorder="0" allowTransparency="true" allow="encrypted-media" />
+      </Grid>
+    </Grid>
   );
 }
